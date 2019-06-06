8995aaf419d7b79857d70638850fa83ff7985a93
amoRNizam <mazin.roma1992@yandex.ru>
2019-06-06 17:52:47 +0300

    123

--- src/main/java/ui/pages/Search.java
+++ src/main/java/ui/pages/Search.java
@@ -1,12 +1,15 @@
 package ui.pages;
 
 import base.WebTestRunner;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import static base.constants.Colors.*;
 import static base.utils.Wait.waitElement;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class Search extends WebTestRunner {
 
@@ -20,6 +23,8 @@ public class Search extends WebTestRunner {
 
     @FindBy(xpath = "(//h2[contains(@class, 'organic__title-wrapper')]//child::a)[last()-1]")
     private WebElement penultimateItemInTheList;
+
+    public ElementsCollection elementsList = $$(".serp-item h2>a");
 
     public void search(String searchText) {
         waitElement(inputSearch, webDriver);
@@ -37,5 +42,9 @@ public class Search extends WebTestRunner {
         System.out.println("\n\n"
                 + ANSI_RED + "Вот она ссылка: " + ANSI_RESET + link + "\n\n");
         penultimateItemInTheList.click();
+    }
+
+    public void displaysReferencePenultimateElementList2() {
+        elementsList.last(2);
     }
 }
